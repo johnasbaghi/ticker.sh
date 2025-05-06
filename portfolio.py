f12bc4e3e3be46c98dc4c16ebb81cd326d94e71c
Yashar Asbaghi <johnasbaghi@gmail.com>
2025-05-06 16:54:34 -0400

    Initial

---
+++ portfolio.py
@@ -0,0 +1,32 @@
+import yfinance as yf
+from tabulate import tabulate
+from simple_colors import red, green
+
+SYMBOLS = ["VOO", "SPY", "VT", "QQQ", "SCHD", "VGK", "XLE", "VB", "VTSAX", "VTI", "MKL", "RKLB", "CNX", "CLF", "HCC", "ADM", "ITA", "IAT", "BAM", "BN", "EWZ", "GLD", "BTC-USD", "CL=F", "GC=F", "SI=F", "ZIM", "TSLA", "IBIT", "FBTC", "MSTR", "IDGT"]
+
+HEADERS=['Symbol', 'Price', '$ Change', '% Change']
+
+def round_to_two_decimals(number):
+  """Rounds a number to two decimal places and returns a string representation,
+  including trailing zeros.
+  """
+  return "{:.2f}".format(number)
+
+rows = []
+for sym in SYMBOLS:
+    dat = yf.Ticker(sym)
+    current_price = dat.info['regularMarketPrice']
+    previous_close = dat.info['previousClose']
+    price_change = round(current_price - previous_close, 2)
+    percent_change = round((current_price - previous_close) / previous_close * 100, 2)
+    
+    row = [sym, round_to_two_decimals(current_price),
+           round_to_two_decimals(price_change),
+           round_to_two_decimals(percent_change)]
+
+    row = [red(x) for x in row] if price_change < 0 else [green(x) for x in row]
+
+    rows.append(row)
+
+
+print(tabulate(rows, HEADERS, disable_numparse=True, colalign=("left", "right", "right", "right")))
